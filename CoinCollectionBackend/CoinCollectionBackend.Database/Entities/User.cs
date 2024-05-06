f6d1bdbd40d8e1a4afca50828609b28fa3b4323e
Schwarcz Florian <florian.schwarcz@gmx.at>
2024-05-06 14:57:41 +0200

    Fixed missing package and user

---
+++ CoinCollectionBackend/CoinCollectionBackend.Database/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCollectionBackend.Database.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string PWHash { get; set; }
+        public required string Salt { get; set; }
+    }
+}
